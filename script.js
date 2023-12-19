cbc4b0a0def0937741c9043503f8ed3f1d125e48
AdreanRodriguez <loco_bladare@hotmail.com>
2023-12-19 08:39:50 +0100

    G uppgift klar

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // G-Version - Split the Nota
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
+Totalsumma (1000kr) ska delas med 4 st vänner som ger en total dricks på 10% (0.10)
 
+START
+
+FUNCTION named "splitTheNota"(totalSumma, antalPersoner, dricksAndel)
+
+    dricksPerPerson = totalSumma * (1 + dricksAndel) / antalPersoner
+
+    //Returnera totala beloppet per person
+    RETURN totalBeloppPerPerson
+ENDFUNCTION
+
+
+//Programmet
+SET totalSumma = 1000
+SET antalPersoner = 4
+SET dricksAndel = 0.10
+
+//Anropa funktionen och spara resultatet i en variabel
+SET beloppPerPerson = splitTheNota(totalSuma, antalPersoner, dricksAndel)
+
+//Resultatet
+PRINT "Varje person ska betala:" + beloppPerPerson + SEK
+
+END
 
 */
 
