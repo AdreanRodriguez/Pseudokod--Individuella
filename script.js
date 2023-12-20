7215a6e3a1f329ca390521d71e38b50a0d2b499a
AdreanRodriguez <loco_bladare@hotmail.com>
2023-12-20 15:55:51 +0100

    done

--- script.js
+++ script.js
@@ -33,19 +33,53 @@ END
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
 
-function play()
-    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
-    SET variabel startOrd till "FOUR";
-    SET variabel slutordOrd till "FIVE";
 
+function play() // Starta programmet
+    SET startOrd till "FOUR"
+    SET slutOrd till "FIVE"
+    SET nuvarandeOrd till ""
+    SET ordLista till [..., ..., ...] // Ordlista
+
+    WHILE loopa tills startOrd är lika med slutOrd
+        
+        PRINT "Ange ett ord: "
+        INPUT nuvarandeOrd
+
+        // Kolla om ordet finns med i ordboken, alltså listan [..., ..., ...]
+        IF nuvarandeOrd INTE finns i ordLista ELLER nuvarandeOrd är längre/kortare än startOrd
+            PRINT "Ingen lösning hittades.";
+        END IF
+
+        ELSE
+            // Kolla om endast ett tecken får ändras
+            IF antalOlikaTecken(nuvarandeOrd, startOrd) inte är lika med 1
+                PRINT "Endast ett tecken får lov att ändras!"
+            END IF
+            ELSE 
+                // Ersätt startOrd med nuvarandeOrd
+                SET startOrd till nuvarandeOrd
+                PRINT "Du är ett steg närmare."
+            END ELSE
+        END ELSE
+    END WHILE
+    PRINT "Grattis!"
 
 end function
 
-function isOneLetterApart(wordOne, wordTwo)
-    SET variabel diffCount till 0;
+// Funktion för att räkna antalet olika tecken mellan två ord
+function antalOlikaTecken(ord1, ord2)
+    SET diffCount till 0;
 
+    // Loopa igenom varje position i orden och jämför tecknen
+    FOR varje position i ord1
+        // Jämför tecknen vid samma position
+        IF ord1[position] inte är lika med ord2[position]
+            // Öka diffCount med 1;
+            INCREASE diffCount med 1;
+        END IF
+    END FOR
 
-    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+    // Returnera det räknade antalet olika tecken
+    RETURN diffCount;
 end function
-
 */
